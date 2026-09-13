439276d0e6fa2533495aa7fd21668e73dfff044f
Clinton Munene <clintonmunene2000@gmail.com>
2026-09-13 12:04:54 +0200

    Data cleaning function

--- scripts/ReusableFunc.py
+++ scripts/ReusableFunc.py
@@ -7,3 +7,11 @@ data_path = Path(r"C:\Users\Hi\Desktop\Risk-Analysis\data\raw\healthcare_patient
 def load_data(data_path):
     data = pd.read_csv(data_path)
     return data
+
+
+# cleaned dataset
+clean_data_path = Path(r"C:\Users\Hi\Desktop\Risk-Analysis\data\processed\PrData.csv")
+
+def load_clean_data(clean_data_path):
+    clean_data = pd.read_csv(clean_data_path)
+    return clean_data
